57229e015aa8b9342cab47c40c88dfbd64ad54e8
jasur0697 <nurmamatov1997@bk.ru>
2021-08-07 13:56:32 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 // filter-btn__close
-var elsCatigoryCheckBtn = document.querySelectorAll(".catigory__check-btn");
-var elsCatigoryChecked = document.querySelectorAll(".catigory__checked");
+// var elsCatigoryCheckBtn = document.querySelectorAll(".catigory__check-btn");
+// var elsCatigoryChecked = document.querySelectorAll(".catigory__checked");
 
-elsCatigoryCheckBtn.forEach(function (list) {
-  list.addEventListener('click', function () {
-    list.closest("p").remove(".catigory__checked");
-  });  
-});
+// elsCatigoryCheckBtn.forEach(function (list) {
+//   list.addEventListener('click', function () {
+//     list.closest("p").remove(".catigory__checked");
+//   });  
+// });
+
+
+
+function myFunction() {
+  document.getElementById("myForm").reset();
+}
