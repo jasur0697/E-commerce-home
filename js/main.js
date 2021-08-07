c16ab87095726c6ee8034168ead2c499f7ec9b06
jasur0697 <nurmamatov1997@bk.ru>
2021-08-07 19:47:23 +0500

    commit

--- js/main.js
+++ js/main.js
@@ -8,3 +8,54 @@
 //   });  
 // });
 
+
+
+var elListViewButton = document.querySelector('.js-list-view-button');
+    var elGridViewButton = document.querySelector('.js-grid-view-button');
+    var elProducts = document.querySelector('.products');
+    var elProductsList = elProducts.querySelector('.products__list');
+    var elsProduct = elProductsList.querySelectorAll('.product');
+
+    function switchToProductsListView () {
+      elProducts.classList.remove('products--grid');
+      elProductsList.classList.remove('products__list--grid');
+      elProductsList.classList.add('products__list--list');
+
+      elsProduct.forEach(function (elProduct) {
+        elProduct.classList.remove('product--grid');
+        elProduct.classList.add('product--list');
+      });
+    }
+
+    function switchToProductsGridView () {
+      elProductsList.classList.remove('products__list--list');
+      elProducts.classList.add('products--grid');
+      elProductsList.classList.add('products__list--grid');
+
+      elsProduct.forEach(function (elProduct) {
+        elProduct.classList.remove('product--list');
+        elProduct.classList.add('product--grid');
+      });
+    }
+
+    if (elListViewButton) {
+      elListViewButton.addEventListener('click', switchToProductsListView);
+    }
+
+    if (elGridViewButton) {
+      elGridViewButton.addEventListener('click', switchToProductsGridView);
+    }
+
+
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+          books:[
+            {
+              title: "Harry Potter",
+              rating: 4
+            }
+          ]
+        }
+      })
